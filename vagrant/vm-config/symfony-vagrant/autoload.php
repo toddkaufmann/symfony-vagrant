70e789ef1547596d549cc907b513b55fffa184be
Todd Kaufmann <toddkaufmann+github@gmail.com>
2015-08-02 17:11:33 -0400

    couple last places where name needs to be changed.

--- vagrant/vm-config/symfony-vagrant/autoload.php
+++ vagrant/vm-config/symfony-vagrant/autoload.php
@@ -7,8 +7,7 @@ use Composer\Autoload\ClassLoader;
  * @var ClassLoader $loader
  */
 // $loader = require __DIR__.'/../vendor/autoload.php';
-//$loader = require '/home/symtest/symtest/vendor/autoload.php';
-$loader = require '/home/vagrant/symtest/vendor/autoload.php';
+$loader = require '/home/vagrant/symProject/vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
