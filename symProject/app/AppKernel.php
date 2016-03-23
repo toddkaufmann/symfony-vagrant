3fcfbba3204a99e098ff6f76a73ec8615b6d5c09
Todd Kaufmann <toddkaufmann+github@gmail.com>
2016-03-23 11:13:55 -0400

    not sure why I had to change this to make it work again (something in 2.7.10 changed dir structure ?)

--- symProject/app/AppKernel.php
+++ symProject/app/AppKernel.php
@@ -33,4 +33,45 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    // customizations:  use dirs inside the VM for cache, logs
+    private $devDirRoot;
+    private $vmRoot;
+  public function __construct($environment, $debug) {
+    parent::__construct($environment, $debug);
+    error_log("AppKernel:  my __DIR__ is: " . __DIR__  . "  -- and file is " . __FILE__ );
+    $this->vmRoot = dirname($_SERVER['SCRIPT_FILENAME']) . '/../app';
+    error_log(" do i want?:  " . $this->vmRoot  );
+    // http://symfony.com/doc/current/cookbook/configuration/override_dir_structure.html
+    // use a different directory for apache v. 'app/console server:run' invocation
+    if ( file_exists("/dev/shm") ) {
+      // linux; maybe vagrant -- ram disk is faster for cache & logs
+      $this->devDirRoot = "/dev/shm/" . getenv("USER") . '/';
+    } else {
+      // do we need USER to distinguish otherwise?  
+      $this->devDirRoot = $this->rootDir . '/';  
+    }
+  }
+
+    public function getRootDir()
+    {
+      // ended up hardcoding it
+      // why does setting above not work ?
+      //  $this->vmRoot; // __DIR__;
+      return '/home/vagrant/symProject/app';
+    }
+
+    public function getCacheDir()
+    {
+      return $this->devDirRoot . '/cache/'.$this->getEnvironment();  // dirname(__DIR__)
+    }
+
+    // or is it easier for developer to just have this available in the vm ?
+    public function getLogDir()
+    {
+        return $this->devDirRoot.'/logs';
+    }
+
+
+
 }
